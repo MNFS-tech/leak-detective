a6c5eb9c698c19096481ed867ec884125448976b
MNFS-tech <b42late.moll@gmail.com>
2025-08-24 00:22:29 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+title: "Leak Detective",
+description: "Training Simulator for Water Leak Detection (SDG 6)",
+};
+
+
+export default function RootLayout({
+children,
+}: {
+children: React.ReactNode;
+}) {
+return (
+<html lang="en">
+<body className="bg-slate-100 min-h-dvh">{children}</body>
+</html>
+);
+}
